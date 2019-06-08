f01d87d00ea81d5c95015d1051c6be83c014d90b
goertzel <goertzel@oregonstate.edu>
2019-06-08 12:51:01 -0700

    Added schemas

--- models/assignments.js
+++ models/assignments.js
@@ -1,1 +1,24 @@
 const { getDBReference } = require('../lib/mongo');
+
+/*
+ * Schema for an Assignment.
+ */
+const AssignmentSchema = {
+	courseId:	{ required: true },
+	title:		{ required: true },
+	points:		{ required: true },
+	due:		{ required: true }
+}
+exports.AssignmentSchema = AssignmentSchema;
+
+/*
+ * Schema for a Submission.
+ */
+const SubmissionSchema = {
+	assignmentId:	{ required: true },
+	studentId:		{ required: true },
+	timestamp:		{ required: true },
+	file:			{ required: true }
+}
+exports.SubmissionSchema = SubmissionSchema;
+
